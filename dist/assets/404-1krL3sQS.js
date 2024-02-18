14f408263364e271ed02f9ff7ebac5aa90c78a74
Destiny Saturday <yiradesat@gmail.com>
2024-02-18 17:19:30 +0100

    chore: build

--- dist/assets/404-1krL3sQS.js
+++ dist/assets/404-1krL3sQS.js
@@ -1,6 +1,1 @@
-import { j as t } from "./index-3AYkAJmI.js";
-import "./react-vendor-D0AUhhr_.js";
-function i() {
-  return t.jsx("div", { children: "404" });
-}
-export { i as default };
+import{j as t}from"./index-3AYkAJmI.js";import"./react-vendor-D0AUhhr_.js";function i(){return t.jsx("div",{children:"404"})}export{i as default};
