663e664c546db12fb97ab88fdc3e53c6e362304f
Destiny Saturday <yiradesat@gmail.com>
2024-02-25 12:23:49 +0100

    fix:  restore husky settings, and lint issues

--- dist/assets/index-B0O_hUbD.js
+++ dist/assets/index-B0O_hUbD.js
@@ -1,1 +1,119 @@
-import{j as e}from"./index-3AYkAJmI.js";import"./react-vendor-D0AUhhr_.js";function l(){return e.jsx("div",{className:"h-screen w-full m-0",children:e.jsx("div",{className:"bg-white h-screen flex justify-center items-center",children:e.jsxs("div",{className:"w-full max-w-md border-none sm:border-gray-200 sm:border-solid sm:rounded-3xl sm:p-8 sm:border p-4",children:[e.jsx("div",{children:e.jsx("h2",{className:"text-2xl font-bold text-center",children:"Get started with us!"})}),e.jsxs("div",{className:"mt-4",children:[e.jsxs("div",{className:"flex flex-col",children:[e.jsx("span",{className:"text-sm",children:"School Name"}),e.jsx("input",{className:"mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",placeholder:"Enter your school name"})]}),e.jsxs("div",{className:"flex flex-col mt-4",children:[e.jsx("span",{className:"text-sm",children:"School Address"}),e.jsx("input",{className:"mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",placeholder:"Enter your school name"})]}),e.jsxs("div",{className:"flex flex-col mt-4",children:[e.jsx("span",{className:"text-sm",children:"Email Address"}),e.jsx("input",{className:"mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",placeholder:"Enter your school name"})]}),e.jsxs("div",{className:"flex flex-col mt-4",children:[e.jsx("span",{className:"text-sm",children:"School Name"}),e.jsx("input",{className:"mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",placeholder:"Enter your school name"})]}),e.jsxs("div",{className:"flex flex-col mt-4",children:[e.jsx("span",{className:"text-sm",children:"Phone Number"}),e.jsx("input",{className:"mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",placeholder:"Enter your school name"})]}),e.jsxs("div",{className:"flex flex-col mt-4",children:[e.jsx("span",{className:"text-sm",children:"Preferred Subdomain"}),e.jsx("input",{className:"mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",placeholder:"Enter your school name"})]})]}),e.jsx("div",{className:"mt-5",children:e.jsx("button",{className:"h-12 w-full bg-[#5573EF] text-white-50 rounded-xl",children:"Next"})})]})})})}export{l as default};
+import { j as e } from "./index-3AYkAJmI.js";
+import "./react-vendor-D0AUhhr_.js";
+function l() {
+  return e.jsx("div", {
+    className: "h-screen w-full m-0",
+    children: e.jsx("div", {
+      className: "bg-white h-screen flex justify-center items-center",
+      children: e.jsxs("div", {
+        className:
+          "w-full max-w-md border-none sm:border-gray-200 sm:border-solid sm:rounded-3xl sm:p-8 sm:border p-4",
+        children: [
+          e.jsx("div", {
+            children: e.jsx("h2", {
+              className: "text-2xl font-bold text-center",
+              children: "Get started with us!",
+            }),
+          }),
+          e.jsxs("div", {
+            className: "mt-4",
+            children: [
+              e.jsxs("div", {
+                className: "flex flex-col",
+                children: [
+                  e.jsx("span", {
+                    className: "text-sm",
+                    children: "School Name",
+                  }),
+                  e.jsx("input", {
+                    className:
+                      "mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",
+                    placeholder: "Enter your school name",
+                  }),
+                ],
+              }),
+              e.jsxs("div", {
+                className: "flex flex-col mt-4",
+                children: [
+                  e.jsx("span", {
+                    className: "text-sm",
+                    children: "School Address",
+                  }),
+                  e.jsx("input", {
+                    className:
+                      "mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",
+                    placeholder: "Enter your school name",
+                  }),
+                ],
+              }),
+              e.jsxs("div", {
+                className: "flex flex-col mt-4",
+                children: [
+                  e.jsx("span", {
+                    className: "text-sm",
+                    children: "Email Address",
+                  }),
+                  e.jsx("input", {
+                    className:
+                      "mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",
+                    placeholder: "Enter your school name",
+                  }),
+                ],
+              }),
+              e.jsxs("div", {
+                className: "flex flex-col mt-4",
+                children: [
+                  e.jsx("span", {
+                    className: "text-sm",
+                    children: "School Name",
+                  }),
+                  e.jsx("input", {
+                    className:
+                      "mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",
+                    placeholder: "Enter your school name",
+                  }),
+                ],
+              }),
+              e.jsxs("div", {
+                className: "flex flex-col mt-4",
+                children: [
+                  e.jsx("span", {
+                    className: "text-sm",
+                    children: "Phone Number",
+                  }),
+                  e.jsx("input", {
+                    className:
+                      "mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",
+                    placeholder: "Enter your school name",
+                  }),
+                ],
+              }),
+              e.jsxs("div", {
+                className: "flex flex-col mt-4",
+                children: [
+                  e.jsx("span", {
+                    className: "text-sm",
+                    children: "Preferred Subdomain",
+                  }),
+                  e.jsx("input", {
+                    className:
+                      "mt-1 h-8  rounded-lg border indent-2.5 border-black border-gray",
+                    placeholder: "Enter your school name",
+                  }),
+                ],
+              }),
+            ],
+          }),
+          e.jsx("div", {
+            className: "mt-5",
+            children: e.jsx("button", {
+              className: "h-12 w-full bg-[#5573EF] text-white-50 rounded-xl",
+              children: "Next",
+            }),
+          }),
+        ],
+      }),
+    }),
+  });
+}
+export { l as default };
