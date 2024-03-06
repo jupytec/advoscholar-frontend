8cea7c89e4bd62506534a900c9e39d7688836598
Captressketh001 <shininglite2012@gmail.com>
2024-03-06 15:30:40 +0100

    restructured folder

--- backend/core/apps.py
+++
@@ -1,8 +0,0 @@
-"""Core app config."""
-from django.apps import AppConfig
-
-
-class CoreConfig(AppConfig):
-    """Core app config."""
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core"
